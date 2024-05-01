e4c230e29c3783c1c95bfc47840eacee6cbbf1af
Leandro <123456@gmail.com>
2024-04-30 21:19:46 -0300

    COMENTARIO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"); //importa o módulo express neste arquivo
 const app = express(); //iniciando o express
 
-
+//Atividade 
 //COMMIT 2
 //criando a rota inicial
 app.get("/", function(req,res){
